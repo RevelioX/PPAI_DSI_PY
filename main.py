6f031622ea289f77bd520480865e186977de4a79
Agustin Nicolas Colodny <agustincolodny@gmail.com>
2023-05-21 17:23:09 -0300

    preguntass

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from Entity import Llamada, CambioEstado, Pregunta
 
 
 def main():
-    #llamadas = Llamada.generarLlamada()
-    #CambioEstado.obtenerFechaRandom()
-    #pantalla = pantallaMain.PantallaConsultarEncuesta()
-    #gestor = ControladorConsultarEncuesta.ControladorConsultarEncuesta(2, 3, llamadas,pantalla)
-
-    #pantalla.opcionConsultarEncuesta(gestor)
-    print(Pregunta.generarPreguntasAleatorias(3))
+    llamadas = Llamada.generarLlamada()
+    CambioEstado.obtenerFechaRandom()
+    pantalla = pantallaMain.PantallaConsultarEncuesta()
+    gestor = ControladorConsultarEncuesta.ControladorConsultarEncuesta(2, 3, llamadas,pantalla)
+    pantalla.opcionConsultarEncuesta(gestor)
+    #print((Pregunta.generarPreguntasAleatorias(3)[0]).getDescripcion())
 
 
 if __name__ == "__main__":
